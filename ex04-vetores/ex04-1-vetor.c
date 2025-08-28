ad7420a04d9ed3f979877a71dcb808ab5d024119
Marcio Bueno <marciobueno@gmail.com>
2025-08-28 11:34:06 +0000

    Modularização do exemplo de vetor

--- ex04-vetores/ex04-1-vetor.c
+++ ex04-vetores/ex04-1-vetor.c
@@ -2,21 +2,39 @@
 
 #define TAM 5
 
+void preencherVetorDigitacao(int vetor[], int tam);
+double calcularMediaVetor(int vetor[], int tam);
+void imprimirValoresAcimaValor(int vetor[], int tam, double valor);
+
 int main() {
   int numeros[TAM];
-  int soma = 0;
-  double media;
-  for (int i = 0; i < TAM; i += 1) {
-    printf("Digite o %dº número inteiro: ", i + 1);
-    scanf("%d", &numeros[i]);
-    soma += numeros[i];
-  }
-  media = (double) soma / (double) TAM;
+  preencherVetorDigitacao(numeros, TAM);
+  double media = calcularMediaVetor(numeros, TAM);
   printf("A média dos números digitados é %.2f\n", media);
+  printf("\nImpressão de todos os números acima da média\n");
+  imprimirValoresAcimaValor(numeros, TAM, media);
+  return 0;
+}
+
+void preencherVetorDigitacao(int vetor[], int tam) {
+  for (int i = 0; i < tam; i += 1) {
+    printf("Digite o %dº número inteiro: ", i + 1);
+    scanf("%d", &vetor[i]);
+  }
+}
+
+double calcularMediaVetor(int vetor[], int tam) {
+  double soma = 0.0;
   for (int i = 0; i < TAM; i += 1) {
-    if (numeros[i] > media) {
-      printf("numeros[%d] = %d\n", i + 1, numeros[i]);
+    soma += vetor[i];
+  }
+  return soma / TAM;
+}
+
+void imprimirValoresAcimaValor(int vetor[], int tam, double valor) {
+  for (int i = 0; i < TAM; i += 1) {
+    if (vetor[i] > valor) {
+      printf("vetor[%d] = %d\n", i + 1, vetor[i]);
     }
   }
-  return 0;
 }
